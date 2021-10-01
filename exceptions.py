4c578759f5742e1960518b9d483d93417664e976
David Carpenter <carpenterd777@gmail.com>
2021-10-01 19:13:56 -0400

    add custom exception

---
+++ exceptions.py
@@ -0,0 +1,11 @@
+"""
+Exceptions to be raised by the data entry
+helper program.
+"""
+
+
+class DataEntryHelperException(Exception):
+    """
+    An exception raised by the data entry
+    helper program.
+    """
