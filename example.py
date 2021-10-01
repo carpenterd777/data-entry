2c9513c7eb969cfe1a9cfc4898a49b43d5f3197a
David Carpenter <carpenterd777@gmail.com>
2021-10-01 18:00:50 -0400

    initial commit

---
+++ example.py
@@ -0,0 +1,8 @@
+"""
+Name        : [dont leave me blank]
+Author      : David Carpenter
+Date        : [dont leave me blank]
+Description : [dont leave me blank]
+Pledge      : I pledge my honor that I have abided by the Stevens Honor
+              System.
+"""
