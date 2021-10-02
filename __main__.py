3c184789e20b1fe468b35a4ac29c7e14cacc83f5
David Carpenter <carpenterd777@gmail.com>
2021-10-02 00:22:11 -0400

    first iteration of data entry instructor

---
+++ __main__.py
@@ -0,0 +1,66 @@
+"""
+Name        : raiser.py
+Author      : David Carpenter
+Date        : 2021-10-01
+Description : A program to assist me in entering data into a database.
+"""
+
+import argparse
+import csv
+import pathlib
+from typing import List
+
+from exceptions import DataEntryHelperException
+from instruct_module import instruct
+
+
+def validate_path(path_as_string: str) -> pathlib.Path:
+    """
+    Ensures that the passed file name exists
+    and is of the appropriate file type.
+    """
+    path = pathlib.Path(path_as_string)
+
+    if not path.exists():
+        raise DataEntryHelperException(f"the path {path_as_string} does not exist")
+
+    if path.suffix != ".csv":
+        raise DataEntryHelperException(f"the extension {path.suffix} is not supported")
+
+    return path
+
+
+def get_data(path: pathlib.Path) -> List[dict]:
+    """
+    Gets all data from the passed csv file
+    and returns a list of all of the entries.
+    """
+    data = []
+    with path.open() as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
+
+
+def main() -> None:
+    """
+    Main method of the program.
+    """
+    parser = argparse.ArgumentParser(
+        description="Step-by-step instructions on what data to enter into the database."
+    )
+    parser.add_argument("path", type=str, help="path to the csv file containing info")
+    args = parser.parse_args()
+    path_as_string: str = args.path
+
+    try:
+        path = validate_path(path_as_string)
+        data = get_data(path)
+        instruct(data)
+    except DataEntryHelperException as ex:
+        parser.error(ex)
+
+
+if __name__ == "__main__":
+    main()
--- commondata.py
+++ commondata.py
@@ -6,9 +6,24 @@ names.
 
 from typing import List
 
+
 class CommonFieldNames:
+    """
+    A collection of field names that are
+    common and generalized for multiple documents
+    in the data entry that I plan to do.
+    """
+
     LAST_NAME = "last name"
     FIRST_NAME = "first name"
+    EMAIL_ADDRESS = "email address"
+    MAIN_NUMBER = "main number"
+    CELL_NUMBER = "cell number"
+    ADDRESS = "address"
+    CITY = "city"
+    STATE = "state"
+    ZIP = "zip"
+
 
 class CommonData:
     """
@@ -18,9 +33,33 @@ class CommonData:
     """
 
     _last_name_field_names = [CommonFieldNames.LAST_NAME, "Last Name :: General"]
-    _first_name_field_names = [CommonFieldNames.FIRST_NAME, "First Name :: General"]
+    _first_name_field_names = [
+        CommonFieldNames.FIRST_NAME,
+        "First Name :: General",
+        "\ufeffFirst Name :: General",
+    ]
+    _email_address_field_names = [
+        CommonFieldNames.EMAIL_ADDRESS,
+        "Email Address :: General",
+    ]
+    _main_number_field_names = [
+        CommonFieldNames.MAIN_NUMBER,
+        "Main Contact Number :: General",
+    ]
+    _cell_number_field_names = [
+        CommonFieldNames.CELL_NUMBER,
+        "Cell Phone Number :: General",
+    ]
 
-    def __init__(self, data : dict) -> None:
+    _address_field_names = [CommonFieldNames.ADDRESS, "Address1 :: General"]
+
+    _city_field_names = [CommonFieldNames.CITY, "City :: General"]
+
+    _state_field_names = [CommonFieldNames.STATE, "State :: General"]
+
+    _zip_field_names = [CommonFieldNames.ZIP, "Zip :: General"]
+
+    def __init__(self, data: dict) -> None:
         self.data = data
 
     def has_field_like(self, field: str) -> bool:
@@ -46,15 +85,24 @@ class CommonData:
 
     def _get_field_name_list(self, field: str) -> List[str]:
         # find the array where the given field is entry 0
-        all_predicted_fields = [self._last_name_field_names, self._first_name_field_names]
+        all_predicted_fields = [
+            self._last_name_field_names,
+            self._first_name_field_names,
+            self._email_address_field_names,
+            self._main_number_field_names,
+            self._cell_number_field_names,
+            self._address_field_names,
+            self._city_field_names,
+            self._zip_field_names,
+            self._state_field_names
+        ]
 
         predicted_fields = None
         for fields in all_predicted_fields:
             if fields[0] == field:
                 predicted_fields = fields
 
-        if predicted_fields == None:
+        if predicted_fields is None:
             raise Exception("got field name that is not yet handled by CommonData")
 
         return predicted_fields
-
