4bdd20564ad736de444b1731ce323fa59ad9c039
David Carpenter <carpenterd777@gmail.com>
2021-10-05 08:47:47 -0400

    Add display functions

--- display.py
+++ display.py
@@ -7,7 +7,7 @@ from typing import List
 import json
 
 import click
-from rich import print_json
+from rich import print_json, print
 
 from input_function import wait_for_input
 from commondata import CommonData
@@ -19,17 +19,17 @@ def page_through(data: List[dict]) -> None:
     the next entry.
     """
     click.clear()
-    for entry in data:
-        _page_one(CommonData(entry))
+    for index in range(len(data)):
+        _page_one(CommonData(data[index]), len(data) - index - 1)
 
 
-def _page_one(data: CommonData) -> None:
+def _page_one(data: CommonData, remaining: int) -> None:
     """
     Print out one entry.
     """
 
     # TODO: this should be a method of CommonData
-
     print("Current entry:\n")
+    print(f"Remaining: [red]{remaining}[/red]")
     print_json(json.dumps(data.data))
     wait_for_input()
